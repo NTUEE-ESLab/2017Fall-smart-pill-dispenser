ff3a78bdbc2294e53cc114b6fcbf082d2b178cb1
Zhong-Yi Li <leon129506@gmail.com>
2018-01-13 03:11:34 -0800

    remove gitmodules and move app_server back into server

--- app_server/models/drug.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const drugSchema = mongoose.Schema({
-  drugname: { type: String, unique: true }
-});
-
-const Drug = mongoose.model('Drug', drugSchema);
-module.exports = Drug;
-
