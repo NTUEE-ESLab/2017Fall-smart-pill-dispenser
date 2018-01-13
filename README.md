c9237670074a282947ca554cca8b572e6c682499
Yusheng Lin <shex.ck@gmail.com>
2018-01-13 10:27:41 +0800

    add codes for Arduino and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Smart Pill Dispenser
+# What is Facial Recognizing Drug Dispenser
+Facial Recognizing Drug Dispenser is a smart drug dispensing system which allows patients to get the pills they have to take via face recognizing technique and enables healthcare professionals to dynamically config prescriptions for each of their patients.
+
+# How we make it
+## 0. Overview
+[Github link for our project](https://https://github.com/NTUEE-ESLab/2017Fall-smart-pill-dispenser)
+![](https://my.ntu.edu.tw/Test/20180112173543.jpg)
+p.s. In project demo we use MM chocolate beans to represent real pills.
+## 1. App
+Caregivers can set patient's personal information and prescription in our Mobile App easily through an interactive chatroom.
+![]()
+Login Screen             | Device Menu | Chatroom
+:-------------------------:|:-------------------------:|:-----------
+![](https://i.imgur.com/WRPUbDb.png =225x375)  |![](https://i.imgur.com/Tnj0izh.png =225x375)|![](https://i.imgur.com/Ex1tjVh.png =225x375)
+
+## 2. Raspberry Pi
+An Raspberry Pi is used to recognize patients(by sending photos to the remote server where facial recognition is performed), fetch user prescription from server and send drug delivery signals to Arduino.
+## 3. Server
+A remote server is responsible for facial recognition and data storage. Our server receives photos taken by a Raspberry Pi, identify who is taking the medicine, then returns the corresponding prescription fetched from database to Raspberry Pi.
+## 4. Arduino
+### Main function
+The Arduino drives dispenser(s) to dispense drugs with specified number and types after receiving drug-dispensing requests from Rpi. 
+### How to use it
+#### 1. Connect the pins between Arduino and Rpi
+We use I2C to connect Arduino and Rpi, so
+#### 2. Connect the pins between Arduino and dispenser(s) 
+Folling [this tutorial](http://yehnan.blogspot.tw/2013/09/arduinotower-pro-sg90.html) of SG-90, connect the signal pin, 5V pin, and the GND pin between the Arduino and the dispenser.
+
+In our project we use the digital pin 9 as the signal pin of dispenser A; the digital pin 10 as the signal pin of dispenser B
+#### 3. Upload the code to Arduino
+Upload the code in "arduino.ino" to Arduino.
+#### 4. Check the functionality of Arduino from Rpi side
+With Arduino on, run "i2c-pi-arduino_test.py" in Rpi, and input the numbers of drug A and drug B respectively. You can thus check whether Arduino receive the correct values from the screen output.
+## 5. Dispenser(s) with SG-90 servo motor(s)
+### Main function
+Each time when the dispenser is called by Arduino, it uses its servo motor SG-90 to rotate the round disk on the bottom of its cup by shaking it back and forth continuously( [see this demo video](https://drive.google.com/file/d/12Qg_4VxAPz-3_LBOeaAZ1OhXzHrLSbA5jA/view)), make one pill pass the hole of its cup, and thus finish dispensing one pill of drug.
+![all components of the drug dispenser](https://my.ntu.edu.tw/Test/20180113095200.jpg)
+### How to use it
+#### 1. Assembly all components in **the picture in Main function section** together and you will see the whole structure like this:
+![assembled dispenser](https://my.ntu.edu.tw/Test/20180113095607.jpg)
+![](https://my.ntu.edu.tw/Test/20180113095652.jpg)
+# Results 
+[The operation of drug dispenser](https://photos.app.goo.gl/EeoVgY1gCzidloPn2)
+
+# References
+1. [OpenCV Face Recognizer API](https://docs.opencv.org/3.0-beta/modules/face/doc/facerec/index.html)
---
+++ arduino.ino
@@ -0,0 +1,214 @@
+﻿#include <Wire.h>
+#include <Servo.h> 
+#define SLAVE_ADDRESS 0x04
+//variables for i2c communicating
+static int number = 0;
+
+
+int state = 0;
+
+
+//variables for servo control
+int incomingByte = 0; 
+Servo myservo1;
+Servo myservo2;
+int startPt =  600;
+int endPt = 2300;
+int shift = 200;
+bool increaseAngle1;
+bool increaseAngle2;
+int pillNum1;
+int pillNum2;
+
+
+void setup() {
+  Serial.begin(9600); // start serial for output
+//setup for servo control
+  myservo1.attach(9, 500, 2400); // 修正脈衝寬度範圍
+  myservo1.write(0); // 一開始先置中90度
+  myservo1.writeMicroseconds(650);
+  
+  myservo2.attach(10, 500, 2400); // 修正脈衝寬度範圍
+  myservo2.write(0); // 一開始先置中90度
+  myservo2.writeMicroseconds(650);
+  
+  increaseAngle1 = true;
+  increaseAngle2 = true;
+  delay(3000);
+    
+//setup for i2c communicating
+  pinMode(8, OUTPUT);
+  
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+  
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+  Serial.println("Ready!");
+}
+
+
+void loop() {
+  Serial.print(">>> # of pills of drug A: ");
+  Serial.println(pillNum1);
+  Serial.print(">>> # of pills of drug B: ");
+  Serial.println(pillNum2);
+  int countA = pillNum1;
+  int countB = pillNum2;
+  for(int i = 0; i < max(pillNum1, pillNum2); i++){
+    Serial.print(">>>>> Round ");
+    Serial.println(i);
+    if(countA > 0){
+      Serial.print(">>> shot for A! No.");
+      Serial.println(i);
+      fire1pill_1();
+      countA--;
+    }else{
+      Serial.println(">>> Drug A done!");
+    }
+    if(countB > 0){
+      Serial.print(">>> shot for B! No.");
+      Serial.println(i);
+      fire1pill_2();
+      countB--;
+    }else{
+      Serial.println(">>> Drug B done!");
+    }
+    
+  }
+  pillNum1 = 0;
+  pillNum2 = 0;
+  delay(1000);
+  
+}
+/////////////////////////////////////////////////
+// functions for i2c
+/////////////////////////////////////////////////
+// callback for received data
+void receiveData(int byteCount){
+  while(Wire.available()) {
+      number = Wire.read();
+      Serial.print(">>> encoded data received: ");
+      Serial.println(number);
+      pillNum1 = number / 16;
+      pillNum2 = number % 16;
+
+
+  }
+}
+
+
+// callback for sending data
+void sendData(){
+  Wire.write(number);
+}
+
+
+///////////////////////////////////////////////
+// functions for servo control
+///////////////////////////////////////////////
+void fire1pill_1(){
+  get1MM_1(increaseAngle1);
+  if(increaseAngle1){
+    increaseAngle1 = false;
+  }else{
+    increaseAngle1 = true;
+  }
+}
+void fire1pill_2(){
+  get1MM_2(increaseAngle2);
+  if(increaseAngle2){
+    increaseAngle2 = false;
+  }else{
+    increaseAngle2 = true;
+  }
+}
+void get1MM_1(bool isincreaseAngle1){
+  if(isincreaseAngle1){
+    Serial.println(">>> angle ++ ing ");
+    myservo1.writeMicroseconds(endPt - shift);
+    for(int i = endPt - shift; i <= endPt; i+=100){
+      myservo1.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 500)myservo1.writeMicroseconds(i - 100);
+      delay(100);
+    }
+
+
+    for(int i = endPt; i >= endPt - shift; i-=100){
+      myservo1.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 2400)myservo1.writeMicroseconds(i + 100);
+      delay(100);
+    }
+    delay(200);
+    
+  }else{
+    Serial.println(">>> angle -- ing ");
+    myservo1.writeMicroseconds(startPt + shift);
+    for(int i = startPt +  shift; i <= startPt; i-=100){
+      myservo1.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 2400)myservo1.writeMicroseconds(i + 100);
+      delay(100);
+    }
+
+
+    for(int i = startPt; i <= startPt + startPt; i+=100){
+      myservo1.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 500)myservo1.writeMicroseconds(i - 100);
+      delay(100);
+    }
+
+
+    delay(200);
+    
+  }
+  
+}
+void get1MM_2(bool isincreaseAngle2){
+  if(isincreaseAngle2){
+    Serial.println(">>> angle ++ ing ");
+    myservo2.writeMicroseconds(endPt - shift);
+    for(int i = endPt - shift; i <= endPt; i+=100){
+      myservo2.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 500)myservo2.writeMicroseconds(i - 100);
+      delay(100);
+    }
+
+
+    for(int i = endPt; i >= endPt - shift; i-=100){
+      myservo2.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 2400)myservo2.writeMicroseconds(i + 100);
+      delay(100);
+    }
+    delay(200);
+    
+  }else{
+    Serial.println(">>> angle -- ing ");
+    myservo2.writeMicroseconds(startPt + shift);
+    for(int i = startPt +  shift; i <= startPt; i-=100){
+      myservo2.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 2400)myservo2.writeMicroseconds(i + 100);
+      delay(100);
+    }
+
+
+    for(int i = startPt; i <= startPt + startPt; i+=100){
+      myservo2.writeMicroseconds(i); // 直接以脈衝寬度控制
+      delay(100);
+      if(i != 500)myservo2.writeMicroseconds(i - 100);
+      delay(100);
+    }
+
+
+    delay(200);
+    
+  }
+  
+}
